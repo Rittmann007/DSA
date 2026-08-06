fb3a097b28410a95ad27e587ce594c7f348238e1
Ritom Paul <rittmann007@gmail.com>
2026-08-06 12:45:34 +0530

    41

--- 100Qservicebased/Q100.java
+++ 100Qservicebased/Q100.java
@@ -856,6 +856,35 @@ public class Q100 {
             return a;
         }
     }
+
+//42. merge two sorted lists
+     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode(-1);
+        ListNode temp1 = temp;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp1.next = list1;
+                list1 = list1.next;
+                temp1 = temp1.next;
+            } else {
+                temp1.next = list2;
+                list2 = list2.next;
+                temp1 = temp1.next;
+            }
+        }
+        while (list1 != null) {
+            temp1.next = list1;
+            list1 = list1.next;
+            temp1 = temp1.next;
+        }
+        while (list2 != null) {
+            temp1.next = list2;
+            list2 = list2.next;
+            temp1 = temp1.next;
+        }
+        return temp.next;
+    }    
         
     
     public static void main(String[] args) {
