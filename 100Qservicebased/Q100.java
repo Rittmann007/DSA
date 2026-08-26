ae93cfebadcaa68554de7da6601329ae00ebc85d
Ritom Paul <rittmann007@gmail.com>
2026-08-26 13:34:16 +0530

    72.

--- 100Qservicebased/Q100.java
+++ 100Qservicebased/Q100.java
@@ -1889,6 +1889,37 @@ int peek(){
         }
         return backtrack(0, 0, m, n, obstacleGrid, grid);
     }
+
+//72. word search(like rat in a maze)
+    public boolean backtrack(char board[][], String word, int i, int j, int index) {
+        if (index == word.length())
+            return true;
+        if ((i < 0 || j < 0) || (i >= board.length || j >= board[0].length)) {
+            return false;
+        }
+        if (board[i][j] != word.charAt(index)) {// this also returns false for visited cells
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '-';
+        boolean val = backtrack(board, word, i + 1, j, index + 1) ||
+                backtrack(board, word, i, j + 1, index + 1) ||
+                backtrack(board, word, i - 1, j, index + 1) ||
+                backtrack(board, word, i, j - 1, index + 1);
+        board[i][j] = temp;
+        return val;
+
+    }
+    public boolean exist(char[][] board, String word) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (backtrack(board, word, i, j, 0)) {// starting from any cell, ans can be found
+                    return true;
+                }
+            }
+        }
+        return false;
+    }    
     
     public static void main(String[] args) {
         // Your code goes here
